9e35652ee5cbcd04045aa5158ec851e727463fc6
Hector Loustaunau <hectorloul@icloud.com>
2025-10-07 18:24:24 -0700

    🔧 Fix: rutas relativas para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuración para GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/devsono/', // 👈 importante: nombre del repositorio
+  base: './', // 👈 Cambia esto: antes estaba '/devsono/'
 })
